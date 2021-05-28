98b5e07dab41be9aca410b3814d4612f0d3ce57a
cld_rojas <cld.rojasz@gmail.com>
2021-05-27 22:40:02 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  let name = "World";
+</script>
+
+<div class="App">
+  <div class="App-content">
+    <h1>Hello {name}!</h1>
+  </div>
+</div>
+
+<style>
+  .App {
+    text-align: center;
+  }
+  h1 {
+    color: tomato;
+  }
+</style>
