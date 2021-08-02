3c502601c52d244314c6a17f4629c9a90082a3fa
cld_rojas <cld.rojasz@gmail.com>
2021-08-02 12:44:05 -0400

    deleted publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,6 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "index.js",
-        publicPath: "/cldrojas",
     },
     module: {
         rules: [
