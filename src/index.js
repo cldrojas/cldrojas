acbbbabc3ef2fec19ade28d3620fdb66e353819a
cld_rojas <cld.rojasz@gmail.com>
2021-08-02 12:37:52 -0400

    changed filename of main.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import App from "./App.svelte";
-import "./global.css";
+import "./main.css";
 
 const app = new App({
     target: document.getElementById("root"), // entry point in ../public/index.html
